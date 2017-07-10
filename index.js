9fbfc737fb1bd7de8404b9ad00010bf7521ce532
DS\chennakk <kamalakannan.chennakesavan@cdk.com>
2017-07-10 18:57:08 -0500

    final touch

--- index.js
+++ index.js
@@ -2,9 +2,6 @@ const express = require('express')
 const app = express()
 var bodyParser = require('body-parser');
 
-//app.use(bodyParser.json()); // for parsing application/json
-//app.use(bodyParser.urlencoded({ extended: true })); // for parsing application/x-www-form-urlencoded
-
 function rawBody(req, res, next) {
   req.setEncoding('utf8');
   req.rawBody = '';
